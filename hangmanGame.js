d5efd8e087d2bf2845c4e7a13c1f92bd3c6293af
Isai Solis <solis.isai@gmail.com>
2018-11-07 18:35:33 -0600

    added wordBankObject and pseudocode to js file.

---
+++ hangmanGame.js
@@ -0,0 +1,39 @@
+const wordBankObject = {
+    queen: {
+        picture: null,
+        song: null,
+        songclip: null 
+    },
+    aerosmith: {
+        picture: null,
+        song: null,
+        songclip: null 
+    },
+    radiohead: {
+        picture: null,
+        song: null,
+        songclip: null 
+    },
+    ramones: {
+        picture: null,
+        song: null,
+        songclip: null 
+    },
+    prince: {
+        picture: null,
+        song: null,
+        songclip: null 
+    }
+};
+
+// when the user starts the app, the display is initialized and instructions are given
+
+// when the user presses the enter key for the first time, the game begins by displaying a series of blanks
+
+// when the user presses an alphabetical key, the user is notified if the guess was correct or not by updating the screen
+
+// when the user has used up all guesses, the screen is updated and the user is notified of the loss
+
+// when the user has guessed all the words correctly, the screen is updated and the user is notified of the win
+
+// if the user wants to quit, the user can press the ESC key to quit the game
