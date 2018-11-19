ed48988f7d7db1964803d4bfe3e3276ba9df2ab5
Isai Solis <solis.isai@gmail.com>
2018-11-18 20:57:40 -0600

    commit

--- hangmanGame.js
+++ hangmanGame.js
@@ -138,9 +138,7 @@ const updatePage = (guessedLetter) => {
 
 
 document.onload = setupGame();
-// when the user presses the enter key for the first time, the game begins by displaying a series of blanks
 
-// when the user presses an alphabetical key, the user is notified if the guess was correct or not by updating the screen
 document.onkeyup = (event) => {
     guessedLetter = String.fromCharCode(event.keyCode).toLowerCase();
     if (keyboardActive) {
@@ -148,9 +146,3 @@ document.onkeyup = (event) => {
         console.log(guessedLetter);
     };
 };
-
-// when the user has used up all guesses, the screen is updated and the user is notified of the loss
-
-// when the user has guessed all the words correctly, the screen is updated and the user is notified of the win
-
-// if the user wants to quit, the user can press the ESC key to quit the game
